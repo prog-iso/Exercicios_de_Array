cf1baff9df6a0d316b518d4614ac156fbf1d86e4
prog-iso <isoton007@gmail.com>
2026-04-23 16:18:13 -0300

    exercicios completos

--- Questao_91/Questao_91.c
+++ Questao_91/Questao_91.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define tam 50
+
 int main()
 {
     setlocale(LC_ALL,"pt_BR.UTF-8");
@@ -10,29 +12,35 @@ int main()
 
     srand(time(NULL));
 
-    int vetor[50], vetor_rep[50], numero, rep = 0;
+    int vetor[tam];
 
     printf("Vamos verificar a existência de um valor numa lista?\n\n");
     printf("Primeiro, precisamos criar um vetor:");
     
-    for(int i = 0; i < 49; i++)
+    for(int i = 0; i < tam; i++)
     {
         vetor[i] = rand() % 100;
         printf(" %d,", vetor[i]);
     }
-    vetor[49] = rand() % 100;
-    printf(" %d.", vetor[49]);
+    printf("\b.");
 
-    printf("\n\nAgora, vejamos quantos números repetidos temos nesse vetor:\n");
+    printf("\n\n\nAgora, vejamos quantos números repetidos temos nesse vetor:");
 
-    for(int i = 1; i < 50; i++)
+    for(int i = 0; i < tam; i++)
     {
-        if(vetor[i] == vetor[i-1]) rep++;
-        else if(rep > 0)
+        int rep = 0;
+        for(int j = 0; j < tam-1; j++)
         {
-            printf("\n\tO número %d aparece %d vezes da posição %d até %d", vetor[i-1], rep, i - rep, i);
-            rep = 0;
+            if(vetor[i]==vetor[j] && i != j)
+            {
+                if(j < i) break;
+                if(rep == 0) printf("\n\nO número %d aparece pela primeira vez na posição %d;", vetor[i], i+1);
+
+                printf("\n\t-> O número %d se repete pela %dº vez na posição %d;", vetor[i], rep+1, j+1);
+                rep++;
+            }
         }
+        if(rep != 0) printf("\b.");
     }
  
     printf("\n\nAcabei meu trabalho aqui. Feche o programa apertando enter.");
